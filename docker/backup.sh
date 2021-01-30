fd575aa0926e1a817e5fcf9f27a2cc25b7a23661
GraysonCAdams <grayson.adams@turner.com>
2021-01-29 19:25:41 -0500

    Automated Postgres backups

---
+++ docker/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /home/root
+date1=$(date +%Y%m%d-%H%M)
+mkdir pg-backup
+PGPASSWORD="$PG_PASS" pg_dumpall -h $PG_HOST -p $PG_PORT -U $PG_USER > pg-backup/postgres-db.tar
+file_name="pg-backup-"$date1".tar.gz"
+
+#Compressing backup file for upload
+tar -zcvf $file_name pg-backup
+filesize=$(stat -c %s $file_name)
+mfs=10
+
+if [[ "$filesize" -gt "$mfs" ]]; then
+# Uploading to s3
+aws s3 cp pg-backup-$date1.tar.gz $S3_BUCKET
+fi
